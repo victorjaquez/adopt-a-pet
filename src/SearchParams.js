8a6bfa984fad352bfddcdf66df19ee7d4c83d06e
victorjaquez <victor@jaquez.me>
2019-02-20 15:37:45 -0700

    handle input change for search

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,12 +7,19 @@ export default class SearchParams extends React.Component {
     breed: ""
   };
 
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="search-params">
         <label htmlFor="location">
           Location
           <input
+            onChange={this.handleLocationChange}
             id="location"
             value={this.state.location}
             placeholder="Location"
